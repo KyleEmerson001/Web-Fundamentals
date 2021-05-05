435b77395c336de37e0f0eb5e9f5cec22fdb5f14
Kyle Emerson <kyle.emerson1@gmail.com>
2021-05-05 14:14:08 -0700

    terms

---
+++ Python/algos2.js
@@ -0,0 +1,17 @@
+const str1 = "aaaabbcddd";
+function splitString(str){
+    var count = 0
+    function countString(str, letter) {
+        let count = 0;
+
+        for (let i = 0; i < str.length; i++) {
+            if (str.charAt(i) == letter) {
+                count += 1;
+            }
+        }
+
+        return count;
+        
+    }
+}
+console.log(splitString)
---
+++ Python/fundamentals/fundamentals/Function Basics2/Function_Basics_2.py
@@ -0,0 +1,31 @@
+# 1 countdown
+num = ()
+def count_down(num):
+    for num in range(num, -1, -1):
+        print(num)
+count_down(5)
+
+# 2 print and return
+def print_and_return(a,b):
+    print(a)
+    return b
+print_and_return(5,4)
+
+# 3 First plus length
+list = [1,2,3,4,5]
+a = 0
+b = 0
+def first_plus_length():
+    a = list[0]
+    b = len(list)
+    return(a+b)
+print(first_plus_length())
+
+# 4 values greater than second
+list = [1,2,3,4,2,5,1]
+new_list = []
+def values_greater_than_second():
+    for num in range(len(list))
+    
+
+#5 this length, that value
