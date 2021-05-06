8ba41da40decba356c44d886c139adc16654f905
Kyle Emerson <kyle.emerson1@gmail.com>
2021-05-06 14:30:53 -0700

    chaining

--- Python/fundamentals/oop/Chaining Methods/Chaining_Methods.py
+++ Python/fundamentals/oop/Chaining Methods/Chaining_Methods.py
@@ -9,10 +9,12 @@ class User():
         
     def make_deposit(self, amount):
         self.account_balance += amount
+        self.display_user_balance()
         return self
 
     def make_withdrawal(self, amount):
         self.account_balance -= amount
+        self.display_user_balance()
         return self
 
     def print_info(self):
@@ -24,23 +26,8 @@ Naruto = User("Naruto Uzumaki")
 Luffy = User("Luffy Monkey")
 Ichigo = User("Ichigo Kurosaki")
 
-Naruto.print_info()
-Naruto.make_deposit(50)
-Naruto.make_deposit(50)
-Naruto.make_deposit(50)
-Naruto.make_withdrawal(50)
-Naruto.display_user_balance()
-Luffy.print_info()
-Luffy.make_deposit(50)
-Luffy.make_deposit(50)
-Luffy.make_withdrawal(1000)
-Luffy.make_withdrawal(50)
-Luffy.display_user_balance()
-Ichigo.print_info()
-Ichigo.make_deposit(100)
-Ichigo.make_withdrawal(10)
-Ichigo.make_withdrawal(10)
-Ichigo.make_withdrawal(50)
-Ichigo.display_user_balance()
+Naruto.print_info().make_deposit(50).make_deposit(50).make_deposit(50).make_withdrawal(50).display_user_balance()
+Luffy.print_info().make_deposit(50).make_deposit(50).make_withdrawal(1000).make_withdrawal(50).display_user_balance()
+Ichigo.print_info().make_deposit(100).make_withdrawal(10).make_withdrawal(10).make_withdrawal(50).display_user_balance()
 
 
