e46659da704a41e7a871caf9ec0dae3ca98cfbed
KyleEmerson001 <82608312+KyleEmerson001@users.noreply.github.com>
2021-05-14 14:51:01 -0700

    playground

--- Python/flask/fundamentals/Playground/templates/playground.py
+++ Python/flask/fundamentals/Playground/templates/playground.py
@@ -8,18 +8,18 @@ def initial_render():
 
 @app.route("/play")
 def block_render():
-    return render_template('playground.html')
+    return render_template("index.html")
 
 @app.route("/play/<number_of_boxes>")
 def block_repeat(number_of_boxes):
     repeat = int(number_of_boxes)
-    return render_template('playground2.html', repeat=repeat)
+    return render_template('index1.html', repeat=repeat)
 
 @app.route("/play/<number_of_boxes>/<color_change>")
 def box_color(number_of_boxes,color_change):
     repeat = (int(number_of_boxes))
     colorChange = color_change
-    return render_template('playground3.html', repeat = repeat, colorChange = colorChange )
+    return render_template('index2.html', repeat = repeat, colorChange = colorChange )
 
 
 if __name__ == "__main__":
