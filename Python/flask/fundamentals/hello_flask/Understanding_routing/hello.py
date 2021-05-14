598d8491b5de764d3bbe278c1c7250b4c97a0d64
KyleEmerson001 <82608312+KyleEmerson001@users.noreply.github.com>
2021-05-13 22:49:46 -0700

    com

---
+++ Python/flask/fundamentals/hello_flask/Understanding_routing/hello.py
@@ -0,0 +1,30 @@
+from flask import Flask
+app = Flask(__name__)
+@app.route("/")
+def hello():
+    return "Hello World!"
+@app.route('/dojo')
+def dojo():
+    return "Dojo!"
+@app.route("/say <sting:name>")
+def hi():
+    return "Hi + name"
+@app.route('/hello')
+def success():
+    for i in range(0,35):
+        print(i)
+    return "hello"
+@app.route('/bye')
+def success():
+    for i in range(0,80):
+        print(i)
+    return "bye"  
+@app.route('/dogs')
+def success():
+    for i in range(0,99):
+        print(i)
+    return "dogs"  
+
+
+if __name__ =="__main__":
+    app.run(debug=True)
--- Python/flask/fundamentals/hello_flask/hello.py
+++ Python/flask/fundamentals/hello_flask/hello.py
@@ -1,30 +1,7 @@
-from flask import Flask
-app = Flask(__name__)
-@app.route("/")
-def hello():
-    return "Hello World!"
-@app.route('/dojo')
-def dojo():
-    return "Dojo!"
-@app.route("/say <sting:name>")
-def hi():
-    return "Hi + name"
-@app.route('/hello')
-def success():
-    for i in range(0,35)
-        print(i)
-    return "hello"
-@app.route('/bye')
-def success():
-    for i in range(0,80)
-        print(i)
-    return "bye"  
-@app.route('/dogs')
-def success():
-    for i in range(0,99)
-        print(i)
-    return "dogs"  
-
-
-if __name__ =="__main__":
-    app.run(debug=True)
+from flask import Flask  # Import Flask to allow us to create our app
+app = Flask(__name__)    # Create a new instance of the Flask class called "app"
+@app.route('/')          # The "@" decorator associates this route with the function immediately following
+def hello_world():
+    return 'Hello World!'  # Return the string 'Hello World!' as a response
+if __name__=="__main__":   # Ensure this file is being run directly and not from a different module    
+    app.run(debug=True)    # Run the app in debug mode.
