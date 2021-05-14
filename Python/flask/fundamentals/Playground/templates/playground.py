60ed3486d8a5a2fe1b2c213e0039fffe9848c166
KyleEmerson001 <82608312+KyleEmerson001@users.noreply.github.com>
2021-05-14 15:02:03 -0700

    update

--- Python/flask/fundamentals/Playground/templates/playground.py
+++ Python/flask/fundamentals/Playground/templates/playground.py
@@ -1,10 +1,9 @@
   
 from flask import Flask, render_template
 app = Flask(__name__)
-
 @app.route("/")
 def initial_render():
-    return "go to http://localhost:5000/play to have it do something "
+    return "Home page"
 
 @app.route("/play")
 def block_render():
