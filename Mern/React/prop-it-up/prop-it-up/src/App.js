ea123d35bb13850dd265cea84ab4ba3d544de894
KyleEmerson001 <82608312+KyleEmerson001@users.noreply.github.com>
2021-07-12 08:33:30 -0700

    update

--- Mern/React/prop-it-up/prop-it-up/src/App.js
+++ Mern/React/prop-it-up/prop-it-up/src/App.js
@@ -6,8 +6,6 @@ function App() {
     <div className="container">
 <div><Person firstname="Jane" lastname = "Doe" age = "45" haircolor = "Black" /></div>
 <div><Person firstname="John" lastname = "Smith" age = "88" haircolor = "Brown" /></div>
-<div><Person firstname="Millard" lastname = "Fillmore" age = "50" haircolor = "Brown" /></div>
-<div><Person firstname="Maria" lastname = "Smith" age = "62" haircolor = "Brown" /></div>
     </div>
   );
 }
